21aa7ebd193960ed5cc8006072ab29a24d103dec
MAYRA APARECIDA PACHECO VACHIANI <51134897863@info.local>
2026-04-29 10:21:34 -0300

    simple calc

--- simple-calc-main 2/simple-calc-main/js/funcoes.js
+++ simple-calc-main 2/simple-calc-main/js/funcoes.js
@@ -1,52 +1,95 @@
-function calcular() {
-    event.preventDefault();
+async function calcular() {
     //entrada
-    let n1 = parseFloat( document.getElementById('n1').value ) ;
-    let n2 = parseFloat( document.getElementById("n2").value );
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById("n2").value);
     let op = document.getElementById("operacao").value;//soma
     let resultado = null;
-    
-    if( isNaN(n1) || isNaN(n2) ){
+
+    if (isNaN(n1) || isNaN(n2)) {
         document.getElementById('resultado').innerText = 'Preencha todos os números!'
     }
 
 
+
+
+
+
     //processamento
-    if(op == 'soma'){
+    if (op == 'soma') {
         resultado = somar(n1, n2)
         resultado = resultado.toFixed(2);
 
-    } else if(op == 'subtracao') {
+    } else if (op == 'subtracao') {
         resultado = subtrair(n1, n2);
         resultado = resultado.toFixed(2);
 
-    } else if (op == 'multiplicacao'){
+    } else if (op == 'multiplicacao') {
         resultado = multiplicar(n1, n2);
         resultado = resultado.toFixed(2);
 
-    } else if (op == 'divisao'){
+    } else if (op == 'divisao') {
 
-        if(n2 == 0) {
+        if (n2 == 0) {
             resultado = 'Não é um número';
         } else {
             resultado = dividir(n1, n2);
             resultado = resultado.toFixed(2);
         }
-            
+
     } else {
         resultado = "Operação Inválida";
     }
 
+
     //saída
     // console.log(`Resultado da operação: ${resultado}`);
     document.getElementById('resultado').innerHTML = resultado;
+
+
+// saçvar o historico
+
+    const objCAL = {
+        n1: n1,
+        n2: n2,
+        op: operacao,
+        resultado: resultado
+
+    };
+
+    const retorno = await cadastrarNaAPI(objCAL);
+
+    if (retorno) {
+        const tabela = document.getElementById("cadastro");
+
+        tabela.innerHTML +=
+            `<article class="data__card-result">
+    <span><strong>Primeiro Número:</strong>${n1}</span>
+    <span><strong>Segundo Número:</strong>${n2}</span>
+  <span><strong>Operação:</strong>${op}</span>
+    <span><strong>Resultado:</strong>${resultado}</span>
+    </article>`;
+
+        document.getElementById("n1").value = "";
+        document.getElementById("n2").value = "";
+        document.getElementById("resultado").innerHTML = "";
+
+        alert(`equação foi cadastrada no banco:
+                Numero 1: ${n1}
+                Numero 2: ${n2}
+                Resultado: ${resultado}`
+        );
+
+    } else {
+        alert("não foi possivel cadastrar")
+    }
 }
+
 
 /**
  * Função somar recebe 2 valores e retorna a soma dos 
  * dois valores
  */
- function somar(valor1, valor2) {
+function somar(valor1, valor2) {
     return valor1 + valor2;
 }
 
@@ -60,32 +103,61 @@ function multiplicar(valor1, valor2) {
 }
 
 function dividir(valor1, valor2) {
-    if(valor2 == 0) {
+    if (valor2 == 0) {
         return 'Não é um número';
     }
-    
+
     return valor1 / valor2;
 }
 
+document.getElementById('cadastro')
 
-const retorno = cadastrarConta();
-if (retorno) {
-    const dados =  document.getElementById("cadastro");
-contaMat    
-    dados.innerHTML += `<tr>    
-    <td>${n1}</td>
-    <td> ${n2}</td>
-    <td>${op}</td>
-    <td>${resultado}</td>
-    </tr>`;
 
-     document.getElementById("n1").value = "";
-     document.getElementById("n2").value = "";
-      document.getElementById("op").value = "";
-      document.getElementById("resultado").value = "";
 
-     alert(`A conta ${n1} com  ${n2} foi cadastrada`);
-    
-} else {
-    alert(`  Não foi possivel cadastrar no banco  `);
-    }   
+async function cadastrarNaAPI(objCAL) {
+    try {
+        let resposta = await fetch("http://localhost:3000/calc", {
+            method: "POST",
+            body: JSON.stringify(objCAL),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        });
+        return true;
+
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+
+async function buscarResultado() {
+    try {
+        const retorno = await fetch("http://localhost:3000/calc");
+        const dadosRetornados = await retorno.json();
+
+        console.log(dadosRetornados);
+
+        const tabela = document.getElementById("cadastro")
+
+        for (let i = 0; i < dadosRetornados.length; i++) {
+
+            tabela.innerHTML +=
+                `<article class="data__card-result">
+            <span><strong>Primeiro Número:</strong>${dadosRetornados[i].n1}</span>
+            <span><strong>Segundo Número:</strong>${dadosRetornados[i].n2}</span>
+            <span><strong>Operação:</strong>${dadosRetornados[i].op}</span>
+            <span><strong>Resultado:</strong>${dadosRetornados[i].resultado}</span>
+            </article>`;
+
+
+        }
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
